9e68fede4a816a4b1daee5e2af8385c1313d8e7f
Surya Bhakti Kusuma <surya27bk@gmail.com>
2022-07-04 23:29:33 +0700

    configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
