74803b596c3be8250a4c0e0d91f2c541b6d3a0a9
Surya Bhakti Kusuma <surya27bk@gmail.com>
2022-07-04 23:27:10 +0700

    editing file index and App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Academy from "./components/Academy";
+import Footer from "./components/Footer";
+import { Hero } from "./components/Hero";
+import { Navbar } from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App relative">
+      <Navbar />
+      <Hero />
+      <About />
+      <Academy />
+      <Portfolio />
+      <Footer />
     </div>
   );
 }
